5edca389e9e307c8528f49568fc56680d69abf51
Ricardo Pari <rikiricardo16@hotmail.com>
2021-08-09 17:38:50 -0400

    08/09/2021 second component with angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ricardo-comp',
   templateUrl: './test1.component.html',
-  styleUrls: ['./test1.component.css']
+  styleUrls: ['./test1.component.scss']
 })
 export class Test1Component implements OnInit {
 
