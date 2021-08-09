2be209b1291db92c0281fbf62ea6d22f7bd8d88a
RicardoPary <rikiricardo16@hotmail.com>
2021-08-09 17:24:44 -0400

    08/09/2021 first task create a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ricardo-comp',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
