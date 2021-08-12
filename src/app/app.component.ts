155606b96a2384c33e59d74ddd35b6c4dcccf43d
RicardoPary <rikiricardo16@hotmail.com>
2021-08-12 18:17:20 -0400

    08/12/2021 Observer Of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {of} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +16,34 @@ export class AppComponent {
 
   color:string;
 
+  tictock = of([1,2,3,4,5]);
+
   constructor(){
+
+    // PERSON A
+    this.tictock.pipe(
+      map(s => s.join('-')),
+      map(s => s + 'hola'),
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    // PERSON B
+    this.tictock.pipe(
+      filter((v:any) => v[0]%2 === 1)
+    ).subscribe(v => {
+      console.log('PERSON B VIDEO', v);
+    });
+    // PERSON C
+    this.tictock.subscribe(v => {
+      console.log('PERSON C VIDEO', v);
+    });
+
   }
 
 
+  onAddVideo(){
+    
+  }
 
 printDataRicardoComp(event:any){
   console.log('RICARDO COMP:', event);
