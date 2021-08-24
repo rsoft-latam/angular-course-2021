a1c8c965d78f0f894a6172b33efb6152ba50a02a
RicardoPary <rikiricardo16@hotmail.com>
2021-08-24 18:41:01 -0400

    24/08/2021 function pure  and impure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,75 +9,29 @@ import { filter, map, delay } from 'rxjs/operators';
 })
 
 export class AppComponent {
-  title = 'angular2021';
-  myStatus = 'my status';
-
-  sw = true;
-
-  color:string;
-  personASub:Subscription;
-  video = 1;
-  tictock = new BehaviorSubject(this.video);
-
-  @ViewChild('myDiv1') myDiv1: ElementRef;
-  @ViewChild('myDiv2') myDiv2: ElementRef;
-  @ViewChild('myCompRicardo') myCompRicardo: any;
-  
 
   constructor(){
+    this.pure(2,3);
+    this.pure(10,2);
+    this.pure(5,5);
 
-    // PERSON A
-    this.personASub = this.tictock.pipe(
-      filter(s => s%2 === 0)
-    ).subscribe(v => {
-      console.log('PERSON A VIDEO', v);
-    });
-    // PERSON B
-    this.tictock.pipe(
-      delay(4000)
-    ).subscribe(v => {
-      console.log('PERSON B VIDEO', v);
-    });
-    // PERSON C
-    this.tictock.subscribe(v => {
-      console.log('PERSON C VIDEO', v);
-    });
+
+    this.impure(2,3);
+    this.impure(10,2);
+    this.impure(5,5);
   }
-  onAddVideo(){
-    this.video ++
-    this.tictock.next(this.video);    
-  }
-
-  person1Unsubscribe(){
-     this.personASub.unsubscribe();
-     console.log('PERSON A SE DESUSCRIBE')
-  }
-
-printDataRicardoComp(event:any){
-  console.log('RICARDO COMP:', event);
-}
-
-
-printData(event){
-  console.log('CHILD COMP SEND DATA: ', event);
-}
   
 
-test(event){
-  console.log('AAA<zx<zAAAAAAAAAAAAAAAAAAAAAAAAA', event);
-}
+  pure(a:number, b:number){
+    console.log(a + b)
+    return a + b;
+  }
 
-test100(event){
-  console.log('234234234234234', event);
-}
+  impure(a:number, b:number){
+    const aux = Math.random();
+    console.log( a + b + aux)
+    return a + b + aux;
+  }
 
-
-onShowLocalVars(){
-  console.log(this.myDiv1, this.myDiv2, this.myCompRicardo);
-
-  this.myCompRicardo.onClickTest();
-
-  this.myDiv2.nativeElement.value = 'ricardo';
-}
 
 }
