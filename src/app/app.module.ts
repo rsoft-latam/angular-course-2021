6ce5463bc62e4e0ef53279f0a4cfbb75a766e6fc
Ricardo Pari <rikiricardo16@hotmail.com>
2021-08-25 17:48:53 -0400

    25/08/2021 components navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,21 @@ import { SharedModule } from './shared/shared.module';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
+import { View1Component } from './view1/view1.component';
+import {RouterModule, Routes} from "@angular/router";
+import { View2Component } from './view2/view2.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'view1', pathMatch: 'full'
+  },
+  {
+    path: 'view1', component: View1Component
+  },
+  {
+    path: 'view2', component: View2Component
+  }
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +36,9 @@ import { Test3Component } from './test3/test3.component';
     Test2Component,
     Test3Component,
     Directive1Directive,
-    Directive2Directive
+    Directive2Directive,
+    View1Component,
+    View2Component
   ],
   imports: [
     BrowserModule,
@@ -29,7 +46,8 @@ import { Test3Component } from './test3/test3.component';
     HomeModule,
     AdminModule,
     BuyModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
