1f3b20df6c4de2d29887c9435b9d4658bc97f6c1
Ricardo Pari <rikiricardo16@hotmail.com>
2021-08-30 17:57:11 -0400

    30/08/2021 Configuration of ngbootstrap

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgbButtonsModule, NgbModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
@@ -12,7 +14,10 @@ const routes: Routes = [
     LoginComponent
   ],
   imports: [
+    NgbToastModule,
+    NgbButtonsModule,
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,3 +63,7 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+import '@angular/localize/init';
+
