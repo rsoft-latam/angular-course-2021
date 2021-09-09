569be856c43c1cdaefac1ddeea7333161361a215
Ricardo Pari <rikiricardo16@hotmail.com>
2021-09-09 18:43:07 -0400

    09/09/2021 save token into localstorage

--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
@@ -12,11 +13,21 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   public login(body:any):Observable<any>{
-    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body);
+    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(
+      map( (res:any) => {
+        this.authSuccess(res.idToken, res.localId)
+        return res;
+      })
+    );
   }
 
   public signUp(body:any){
     return this.http.post(`${this.url}/v1/accounts:signUp?key=${this.key}`, body);
+  }
+
+  private authSuccess(token:string, userId: string){
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
   }
 
 }
