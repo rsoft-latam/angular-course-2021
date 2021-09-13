5971ca9ce5490dfdb45db3bba91d6c8d47144b67
Ricardo Pari <rikiricardo16@hotmail.com>
2021-09-12 22:11:37 -0400

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./core/interceptors/auth.interceptor";
-import {AuthService} from "./login/services/auth.service";
-
+import {CoreModule} from "./core/core.module";
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', loadChildren:
-      () => import('./login/login.module').then(m => m.LoginModule)},
-  {path: 'pages', loadChildren:
-      () => import('./pages/pages.module').then(m => m.PagesModule)}
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)}
 ];
 
 @NgModule({
@@ -25,16 +20,10 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
-  ],
-  providers: [
-    AuthService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from "./interceptors/auth.interceptor";
+import {AuthService} from "../login/services/auth.service";
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ]
+})
+
+export class CoreModule {
+}
