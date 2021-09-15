9435b4a7ea0d5b5cda6ad10ce26140ba6a341de0
Ricardo Pari <rikiricardo16@hotmail.com>
2021-09-15 17:23:53 -0400

    add methods logout, auto login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {map} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthService {
@@ -10,22 +11,24 @@ export class AuthService {
   url = environment.auth.apiBaseUrl;
   key = environment.auth.key;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
 
-  public login(body:any):Observable<any>{
+  public login(body: any): Observable<any> {
     return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(
-      map( (res:any) => {
+      map((res: any) => {
         this.authSuccess(res.idToken, res.localId)
         return res;
       })
     );
   }
 
-  public signUp(body:any){
+  public signUp(body: any) {
     return this.http.post(`${this.url}/v1/accounts:signUp?key=${this.key}`, body);
   }
 
-  private authSuccess(token:string, userId: string){
+  private authSuccess(token: string, userId: string) {
     localStorage.setItem('token', token);
     localStorage.setItem('userId', userId);
   }
@@ -34,8 +37,20 @@ export class AuthService {
     return localStorage.getItem('token');
   }
 
-  public getUserId(): string | null{
+  public getUserId(): string | null {
     return localStorage.getItem('userId');
+  }
+
+  public verifyLogged(): boolean {
+    const token = localStorage.getItem('token');
+    // token ? true : false
+    return !!token;
+  }
+
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    this.router.navigate(['login'])
   }
 
 }
