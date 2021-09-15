49c74db7346de4b13e748bc896911d46c1b9f51b
Ricardo Pari <rikiricardo16@hotmail.com>
2021-09-15 00:16:00 -0400

    design profile

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import {RouterModule, Routes} from "@angular/router";
+import {SharedMaterialModule} from "../../shared/shared-material.module";
+import {MessageSenderModule} from "../../shared/modules/message-sender/message-sender.module";
+import {PostModule} from "../../shared/modules/post/post.module";
 
 const routes: Routes = [
   {path: '', component: ProfileComponent}
@@ -13,7 +16,10 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    MessageSenderModule,
+    PostModule,
+    RouterModule.forChild(routes),
+    SharedMaterialModule
   ]
 })
 export class ProfileModule { }
